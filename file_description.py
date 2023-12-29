a1d8ca3bdcafa913c517a5409ab710f1d21bcd65
Ameya Patil <101253797+ameya135@users.noreply.github.com>
2023-12-29 19:09:06 +0530

    Update file_description.py

--- file_description.py
+++ file_description.py
@@ -1,11 +1,12 @@
 import autogen
-
+import json
+from os import path
 
 def describe(file_path):
     
     config_list = [
         {
-            "api_key": "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",
+            "api_key": "Enter your api key here",
             "model": "gpt-3.5-turbo"
         }
     ]
@@ -21,17 +22,19 @@ def describe(file_path):
         name="assistant",
         llm_config=llm_config,
         is_termination_msg=lambda x: True if "TERMINATE" in x.get("content") else False,
+        system_message="Describe the code in 3 lines and then type 'TERMINATE' in the same sentence",
     )
     # create a UserProxyAgent instance named "user_proxy"
     user_proxy = autogen.UserProxyAgent(
         name="user_proxy",
         human_input_mode="NEVER",
         is_termination_msg=lambda x: True if "TERMINATE" in x.get("content") else False,
-        max_consecutive_auto_reply=10,
+        max_consecutive_auto_reply=1,
         code_execution_config={
             "work_dir": "work_dir",
             "use_docker": False,
         },
+        system_message="Describe the code in 2 lines then type 'TERMINATE' after the same sentence.",
     )
 
     def extract_code_from_file(file_path):
@@ -48,6 +51,24 @@ def describe(file_path):
 
     task1 = f"Describe the code {code} in 2 lines."
     user_proxy.initiate_chat(assistant, message=task1)
+    return user_proxy,assistant
 
+def update_json(file_name, res):
+    
+    file_name = file_name.split("/")[-1]
+    json_file_path = './file_metadata.json'
+    list_obj = []
 
-#describe('/home/ameya/Documents/model_host/autogen/test/add3.py')
+    if path.isfile(json_file_path) is False:
+        raise Exception("File not found")
+
+    with open(json_file_path) as f:
+        list_obj = json.load(f)
+        
+    list_obj.append({
+        "Name": file_name,
+        "Description": res
+    })
+
+    with open(json_file_path, 'w') as f:
+        json.dump(list_obj, f, indent=4, separators=(',', ': '))
